d3d1e564bc4047980059f330f9e6e0c35b36e64e
pyro <90619153+pyroTM@users.noreply.github.com>
2022-04-30 11:49:51 -0500

    new

---
+++ static/index.js
@@ -0,0 +1,39 @@
+var form = document.getElementById('srchbutton');
+var input = document.getElementById('srchinput');
+
+form.addEventListener('click', async event => {
+    event.preventDefault();
+    window.navigator.serviceWorker.register('./sw.js', {
+        scope: __uv$config.prefix
+    }).then(() => {
+        let url = input.value.trim();
+        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
+        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
+
+
+        if ((localStorage.getItem('aboutBlankCloaking')) === 'true') {
+
+
+            if (win) { win.focus(); } else {
+                win = window.open();
+                win.document.body.style.margin = '0';
+                win.document.body.style.height = '100vh';
+                var iframe = win.document.createElement('iframe');
+                iframe.style.border = 'none';
+                iframe.style.width = '100%';
+                iframe.style.height = '100%';
+                iframe.style.margin = '0';
+                iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+                console.log(iframe.src)
+                win.document.body.appendChild(iframe)
+            }
+        }
+
+        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+    });
+});
+
+function isUrl(val = '') {
+    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
+    return false;
+};
