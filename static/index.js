c093b4d777fae5e9c5586a6883b93f5139ff56b2
pyro <90619153+pyroTM@users.noreply.github.com>
2022-04-30 11:53:15 -0500

    Update index.js

--- static/index.js
+++ static/index.js
@@ -14,19 +14,19 @@ form.addEventListener('click', async event => {
         if ((localStorage.getItem('aboutBlankCloaking')) === 'true') {
 
 
-            if (win) { win.focus(); } else {
-                win = window.open();
-                win.document.body.style.margin = '0';
-                win.document.body.style.height = '100vh';
-                var iframe = win.document.createElement('iframe');
-                iframe.style.border = 'none';
-                iframe.style.width = '100%';
-                iframe.style.height = '100%';
-                iframe.style.margin = '0';
-                iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
-                console.log(iframe.src)
-                win.document.body.appendChild(iframe)
-            }
+
+            win = window.open();
+            win.document.body.style.margin = '0';
+            win.document.body.style.height = '100vh';
+            var iframe = win.document.createElement('iframe');
+            iframe.style.border = 'none';
+            iframe.style.width = '100%';
+            iframe.style.height = '100%';
+            iframe.style.margin = '0';
+            iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
+            console.log(iframe.src)
+            win.document.body.appendChild(iframe)
+
         }
 
         window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
